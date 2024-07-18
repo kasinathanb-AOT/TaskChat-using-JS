e8b38081433bb7f8e37185e168bf40bf1b541e34
kasinathanb-aot <kasinathan@aot-technologies.com>
2024-07-18 04:32:17 -0700

    Final commit

--- demo.js
+++ demo.js
@@ -1,87 +1,87 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const messages = JSON.parse(localStorage.getItem("messages")) || [];
-  const date = new Date();
-  const time = date.getHours() + ":" + date.getMinutes();
-  const msgLen = messages.length;
   const chatarea = document.getElementById("chatarea");
-  const tutorial = document.getElementById("tutorial").textContent =
-    "enter `/m` to see the messages";
+  const tutorialElem = document.getElementById("tutorial");
 
-  // Create new message
+  tutorialElem.textContent = "enter `/m` to see the messages";
+
   const createMsg = (message) => {
-    const messageData = {
-      id: Date.now(),
-      text: message,
-    };
+    const messageData = { id: Date.now(), text: message };
     const send = document.createElement("div");
     send.className = "send";
-    messages.push(messageData);
-    const textMessage = document.createElement("p");
-    textMessage.innerHTML = message;
-    send.appendChild(textMessage);
+    send.innerHTML = `<p>${message}</p>`;
     chatarea.appendChild(send);
+    messages.push(messageData);
     localStorage.setItem("messages", JSON.stringify(messages));
-    if (msgLen != 0) {
-      messages.forEach((i) => {});
-    }
   };
 
-  // Adding new message
-  const sendBtn = document.getElementById("sendBtn");
-  sendBtn.addEventListener("click", function () {
-    const message = document.getElementById("inputMsg").value;
-    if (message != "") {
-      if (message === "/m") {
-        listMessages();
-      } else {
-        createMsg(message);
-      }
-      document.getElementById("inputMsg").value = "";
-    } else {
-      window.alert("Please enter a message");
-    }
-  });
-
-  // Listing all messages
   const listMessages = () => {
     const receivedMsg = document.createElement("div");
     receivedMsg.className = "receive";
     const textMessageUL = document.createElement("ul");
-    messages.map((i,index) => {
-      console.log(i);
+
+    messages.forEach((msg, index) => {
       const textMessageLI = document.createElement("li");
-      textMessageLI.innerHTML = `${i.text}`;
+
+      const textDiv = document.createElement("div");
+      textDiv.className = "textDiv";
+      textDiv.innerText = msg.text;
+
+      const btnDiv = document.createElement("div");
+      btnDiv.className = "btnDiv";
+
       const deleteBtn = document.createElement("button");
       deleteBtn.className = "btn";
-      deleteBtn.innerHTML = `<img src="../images/delete.png" />`;
-      const editBtn = document.createElement("button");
-      editBtn.className = "btn";
-      editBtn.innerHTML = `<img src ="../images/edit.png"/>`;
-      editBtn.addEventListener("click", function () {
-        const editInput = document.createElement("input");
-        const saveBtn = document.createElement("button");
-        saveBtn.innerHTML = "Save";
-        saveBtn.addEventListener("click", function () {
-          const editedMsg = editInput.value;
-          textMessageLI.innerHTML = `${editedMsg}`;
-          localStorage.setItem("messages", JSON.stringify(messages));
-        });
-        editInput.value = i.text;
-        editInput.id = "editInput";
-        textMessageUL.appendChild(editInput);
-        textMessageUL.appendChild(saveBtn);
-        textMessageLI.removeChild(textMessageLI.lastChild);
-      });
-      deleteBtn.addEventListener("click", function () {
+      deleteBtn.innerHTML = `<img src="./images/delete.png" />`;
+      deleteBtn.addEventListener("click", () => {
         messages.splice(index, 1);
         localStorage.setItem("messages", JSON.stringify(messages));
         textMessageUL.removeChild(textMessageLI);
       });
-      textMessageLI.appendChild(deleteBtn);
-      textMessageLI.appendChild(editBtn);
+
+      const editBtn = document.createElement("button");
+      editBtn.className = "btn";
+      editBtn.innerHTML = `<img src="./images/edit.png"/>`;
+      editBtn.addEventListener("click", () => {
+        const editInput = document.createElement("input");
+        const saveBtn = document.createElement("button");
+        saveBtn.innerHTML = "Save";
+        saveBtn.addEventListener("click", () => {
+          const editedMsg = editInput.value;
+          textDiv.innerText = editedMsg;
+          messages[index].text = editedMsg;
+          localStorage.setItem("messages", JSON.stringify(messages));
+          btnDiv.replaceChild(editBtn, saveBtn);
+          btnDiv.removeChild(editInput);
+        });
+        editInput.value = msg.text;
+        btnDiv.appendChild(editInput);
+        btnDiv.replaceChild(saveBtn, editBtn);
+      });
+
+      btnDiv.appendChild(deleteBtn);
+      btnDiv.appendChild(editBtn);
+      textMessageLI.appendChild(textDiv);
+      textMessageLI.appendChild(btnDiv);
       textMessageUL.appendChild(textMessageLI);
     });
+
     receivedMsg.appendChild(textMessageUL);
     chatarea.appendChild(receivedMsg);
   };
+
+  document.getElementById("sendBtn").addEventListener("click", () => {
+    const message = document.getElementById("inputMsg").value;
+    if (message) {
+      if (message === "/m") {
+        messages.length ? listMessages() : (tutorialElem.textContent = "No Data is available");
+      } else {
+        createMsg(message);
+        tutorialElem.textContent = "";
+      }
+      document.getElementById("inputMsg").value = "";
+    } else {
+      alert("Please enter a message");
+    }
+  });
 });
